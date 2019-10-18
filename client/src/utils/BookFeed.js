040846b53760f6ac9585cc775ab733dad4be9a5d
froglegg <hayescrowley@gmail.com>
2019-10-18 13:11:40 -0400

    yep

--- client/src/utils/BookFeed.js
+++ client/src/utils/BookFeed.js
@@ -1,5 +1,5 @@
 import openSocket from "socket.io-client";
-const socket = openSocket("http://localhost:8000");
+const socket = openSocket("");
 
 function subscribeToBookFeed(cb) {
   socket.on("bookFeed", sendFeed => cb(null, sendFeed));
