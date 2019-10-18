cce3cc89bf505e307e977d658c4d51a43f02ce20
froglegg <hayescrowley@gmail.com>
2019-10-18 12:04:17 -0400

    adding socket io again

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./style.css";
-// import BookFeed from "../BookFeed";
+import BookFeed from "../BookFeed";
 function Header() {
   return (
     <div className="headerWrapper">
       <div className="header">
         <h1>MERN Book Search</h1>
       </div>
-      {/* <BookFeed /> */}
+      <BookFeed />
     </div>
   );
 }
--- client/src/utils/BookFeed.js
+++ client/src/utils/BookFeed.js
@@ -1,14 +1,14 @@
-// import openSocket from "socket.io-client";
-// const socket = openSocket("http://localhost:8000");
+import openSocket from "socket.io-client";
+const socket = openSocket("http://localhost:8000");
 
-// function subscribeToBookFeed(cb) {
-//   socket.on("bookFeed", sendFeed => cb(null, sendFeed));
-//   socket.emit("subscribeToBookFeed", 500);
-// }
-// export { subscribeToBookFeed };
+function subscribeToBookFeed(cb) {
+  socket.on("bookFeed", sendFeed => cb(null, sendFeed));
+  socket.emit("subscribeToBookFeed", 500);
+}
+export { subscribeToBookFeed };
 
-// // router.get("/", (req, res) => {
-// //   socket.on("FromAPI", data => res.send({ response: data }));
-// // });
+// router.get("/", (req, res) => {
+//   socket.on("FromAPI", data => res.send({ response: data }));
+// });
 
-// // module.exports = router;
+// module.exports = router;
